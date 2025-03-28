27d89dd0bdc349c6e4308a845b9e9b297abd1c70
Guilherme <51869757+v3ntur4X@users.noreply.github.com>
2025-03-27 22:54:26 -0300

    Add files via upload

---
+++ level3/passcodes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+x=$RANDOM
+echo "Input the random number."
+read input
+
+if [[ "$input" -eq "$x" ]] then
+	echo "AWESOME sauce"
+	cat flag.txt
+else
+	echo "$input"
+	echo "$x try again"
+fi
